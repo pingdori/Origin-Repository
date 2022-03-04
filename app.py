132a37b9bad02bfed262368170aa4145ba09be5a
Doris Li <bubucc0629@gmail.com>
2022-03-05 03:49:27 +0800

    second commit

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import *
+import json
+import mysql.connector
+from flask import jsonify
+from dotenv import load_dotenv
+from collections import defaultdict
+import os
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
-
+load_dotenv()
+MYSQL_HOST=os.getenv("mysql_host")
+MYSQL_PORT=os.getenv("mysql_port")
+MYSQL_USER=os.getenv("mysql_user")
+MYSQL_PASSWORD=os.getenv("mysql_password")
 # Pages
 @app.route("/")
 def index():
@@ -16,5 +26,109 @@ def booking():
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
+	
+@app.route("/api/attractions")
+	
+def apiAttraction():
+	# try:
+	keyword_query = request.args.get('keyword')
+	pageStrquery= str(request.args.get('page'))
+	
+	if 	pageStrquery and 	keyword_query ==None:
+		
+		connection = mysql.connector.connect(host=MYSQL_HOST,port=MYSQL_PORT,user=MYSQL_USER,password=MYSQL_PASSWORD)
+		cursor = connection.cursor()
+		cursor.execute("USE `taipei-attractions`")
+				# selectKeyword="SELECT * from `data` Join `data_images` on `data`.`name`=`data_images`.`name` and `data`.`name` like %s LIMIT 11 OFFSET 0" ,(keyword_query,)
+				# selectPage="SELECT * from `data` Join `data_images` on `data`.`name`=`data_images`.`name` and `data`.`name` LIMIT 11 OFFSET %s" ,(page_query,)
+				# selectKeypage="SELECT * from `data` Join `data_images` on `data`.`name`=`data_images`.`name` and `data`.`name` like %s  LIMIT 11 OFFSET %s;" ,(keyword_query,page_query,)
+		countAll="SELECT count(*) from `data`"
+				# count="SELECT count(*) from `data` WHERE `name` like %s LIMIT 11 OFFSET %s",(keyword_query,page_query,)
+				# if keyword_query ==None:
+				# if pageStrquery==0:
+				# 	pageOffset=0
+				# else:
+				# 	pageOffset=int(pageStrquery)*11
+		ff=[11]
+		cursor.execute("SELECT `data`.`id`,`data`.`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`,`images` from `data`Join  `data_images` on `data`.`name`=`data_images`.`name`")
+		results = cursor.fetchall()
+		jsonA=json.dumps(results)
+		jsonB=json.loads(jsonA) 
+		des=cursor.description
+		desDumps=json.dumps(des)
+		desLoad=json.loads(desDumps) 
+		title=["id","name","category","description","address","transport","mrt","latitude","longitude","images"]
+			
 
+		# dic=dict()
+		# ID=["id"]
+		# name=["name"]
+		# category=["category"]
+		# description=["description"]
+		# address=["address"]
+		# transport=["transport"]
+		# mrt=["mrt"]
+		# latitude=["latitude"]
+		# longitude=["longitude"]
+		# images=["images"]
+		# sum=0
+		# for value in jsonB:
+			
+		# 	while sum<len(value):
+		# 		dic[ID[0]]=str(value[0])
+		# 		dic[name[0]]=str(value[1])
+		# 		dic[category[0]]=str(value[2])
+		# 		dic[description[0]]=str(value[3])
+		# 		dic[address[0]]=str(value[4])
+		# 		dic[transport[0]]=str(value[5])
+		# 		dic[mrt[0]]=str(value[6])
+		# 		dic[latitude[0]]=str(value[7])
+		# 		dic[longitude[0]]=str(value[8])
+		# 		dic[images[0]]=str(value[9])
+		# 		sum+=1
+		# 		print(dic)
+		# 		return(jsonify(dic))
+		# dic=[]
+		# dic1={}
+		# ID="id"
+		# name="name"
+		# category="category"
+		# description="description"
+		# address="address"
+		# transport="transport"
+		# mrt="mrt"
+		# latitude="latitude"
+		# longitude="longitude"
+		# images="images"
+		# IID=desLoad[0]
+		# for value in jsonB:
+		# 	# while sum<len(value):
+		# 		allId=str(value[0])
+		# 		Id=ID[0]+ allId
+		# 		json.dumps(Id)
+				
+
+		# 		# sum+=1
+		# return(jsonify(Id))
+		# except:
+		# 	null = '{"data":null}'
+		# 	return null
+		# finally:
+			# cursor.close()
+			# connection.close()
+					
+			# elif page_query and keyword_query != None:
+			# 	cursor.execute(selectKeypage)
+				
+			# cursor.execute("SELECT * from `data` Join `data_images` on `data`.`name`=`data_images`.`name` and `data`.`name` like %s  LIMIT 11 OFFSET %s;" ,(keyword_query,page_query,))
+			# results = cursor.fetchall()
+		list_data=[]
+		for i in jsonB:
+				co=dict(zip(title,jsonB))
+				x=["data"]
+				d1 = defaultdict(lambda: defaultdict(dict))
+				list_data.append(co)
+				d1["data"]=list_data
+	return(jsonify(d1))
+app.debug = True
 app.run(port=3000)
---
+++ data/mysql_db.py
@@ -0,0 +1,45 @@
+import json
+import mysql.connector
+from flask import jsonify
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+MYSQL_HOST=os.getenv("mysql_host")
+MYSQL_PORT=os.getenv("mysql_port")
+MYSQL_USER=os.getenv("mysql_user")
+MYSQL_PASSWORD=os.getenv("mysql_password")
+
+connection = mysql.connector.connect(host=MYSQL_HOST,port=MYSQL_PORT,user=MYSQL_USER,password=MYSQL_PASSWORD)
+cursor = connection.cursor()
+cursor.execute("USE `taipei-attractions`")
+results = cursor.fetchall()
+with open('taipei-attractions.json',encoding='utf-8') as f:
+    jsonData=json.load(f)
+dataList=jsonData["result"]["results"]
+for i in dataList:
+    name=i["stitle"]
+    category=i["CAT2"]
+    description=i["xbody"]
+    addr=i["address"]
+    address=addr.replace(" ", "")
+    transport=i["info"]
+    MRT=i["MRT"]
+    latitude=i["latitude"]
+    longitude=i["longitude"]
+    imgFile=i["file"]
+    urlSplit=imgFile.split("https")
+    strA=",https".join(urlSplit)
+    strB=strA.split(",")
+    strB.pop(0)
+    
+    filterData=[a for a in strB if ".jpg" in a or ".JPG" in a]
+    images=",".join(filterData)
+    
+    cursor.execute("INSERT INTO `data`(`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`) VALUES(%s, %s, %s, %s, %s, %s, %s, %s,)",(name,category,description,address,transport,MRT,latitude,longitude,))
+    cursor.execute("INSERT INTO `data`(`name`,`category`,`description`,`address`,`transport`,`mrt`,`latitude`,`longitude`) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(name,category,description,address,transport,MRT,latitude,longitude))
+    cursor.execute("INSERT INTO `data_images`(`name`,`images`) VALUES(%s,%s)",(name,images,))
+    connection.commit()
+    cursor.close()
+    connection.close()
+app.run(port=3000)
